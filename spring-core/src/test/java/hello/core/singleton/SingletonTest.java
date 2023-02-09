b0cf89c3792411e5e0f700261111069fecc34679
Jayson <jsoonworld@gmail.com>
2023-02-09 20:57:01 +0900

    core_5th

---
+++ spring-core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,30 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 개겣를 생성
+        MemberService memeberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 개겣를 생성
+        MemberService memeberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memeberService2 = " + memeberService1);
+        System.out.println("memeberService2 = " + memeberService2);
+
+        //memberService != memberService2
+        Assertions.assertThat(memeberService1).isNotSameAs(memeberService2);
+
+
+    }
+}
