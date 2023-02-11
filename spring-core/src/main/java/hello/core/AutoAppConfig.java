0d854fd6671f6366bc3dbc46a3f848f0d5c5494d
Jayson <jsoonworld@gmail.com>
2023-02-12 08:53:58 +0900

    core_6

--- spring-core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        
+
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
