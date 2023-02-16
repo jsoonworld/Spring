ed3d273a9edb37458cfaf261c521dc9b844c96d7
Jayson <jsoonworld@gmail.com>
2023-02-16 13:14:57 +0900

    core_7th

---
+++ spring-core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -0,0 +1,14 @@
+package hello.core.lifeCycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient(String url) {
+        this.url = url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
